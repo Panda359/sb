978bd353a48cb59cccbec592d4b29a98a2a52aa9
Panda359 <jiewjaw@gmail.com>
2020-02-13 01:03:47 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -12,7 +12,7 @@ from googletrans import Translator
 
 botStart = time.time()
 
-cl = LINE()
+cl = LINE("fSTr2G4/t3EQ2vocVfa7xSRDUWDJVkKtPYFOTfQOhSE+H4XS9iir2Scv/jjqNAl4OC3fZlgTSHPvEAv0SC49WkhLza2J3qWAWJ5BotlBD1brgoCWAjJT4KGZDNeABUBMHgcn68Ofb+t7srQviEYjOgdB04t89/1O/w1cDnyilFU=")
 #cl = LINE("TOKENMU")
 #cl = LINE("Email","Password")
 #cl = LINE()
